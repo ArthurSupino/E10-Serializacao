d4c80d2f562ae47c08ff275f0601a981446fc01f
ArthurSupino <161601080+ArthurSupino@users.noreply.github.com>
2024-09-19 07:53:04 -0300

    Delete ValorNegativoException.java

--- ValorNegativoException.java
+++
@@ -1,8 +0,0 @@
-public class ValorNegativoException extends Exception {
-    public ValorNegativoException() {
-        super("Valor negativo não é permitido");
-    }
-    public ValorNegativoException(String message) {
-        super(message);
-    }
-}
